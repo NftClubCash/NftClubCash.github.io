8a7a4399091a4729c28f14ed561bccb25e1ec7e7
MorgenTee <MorgenTee@protonmail.com>
2022-03-15 17:24:07 +0100

    staking website upload

---
+++ js/settings.js
@@ -0,0 +1,10 @@
+// Settings for testnet (SmartBCH Amber, NetID 10001)
+window.TokenAddress     = '0x6eED7e1F219d6436dCfa0860FD9b91363a9de37A';
+window.PixelguyAddress  = '0x3c4f773efe8b0Fc7f0c10CaCd074429536607205';
+window.ShoraiAddress    = '0xbda792DcBEEFFbB066879AB9EF4A444eD6f72729';
+window.PowerAddress     = '0xFe390637d4fcA1AaB8742271Fe24af9fdBbB6BfC';
+window.StakingAddress   = '0x50fb43b4e1089C4fD6673bD22f5FDAA45C133875';
+window.DevAddress       = '0x56CE632320B8aF872b9ef5b94C4a483A63f1EE26';
+window.DevCut           = 0.1;
+window.netId            = 10001;
+window.minHarvest       = 200 * 10 * 18;
